e00292ae2f3bdcb14b9e5ff285b4df445225e3ed
Felipe_Gomez <cristian.gomez.mt@usa.edu.co>
2022-10-13 15:36:49 -0500

    tercer commit

--- src/main/resources/static/funciones.js
+++ src/main/resources/static/funciones.js
@@ -1,29 +1,30 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
- */
-function traerDatosCategory(){
-    $.ajax ({
-                url: "http://localhost:8080/api/Client/all",
-                type: "GET",
-                dataType: "json",
-                succes: function(respuesta){
-                    pintarDatos(respuesta);
-                }
-        
+function traerDatosCat(){
+    $.ajax({
+        url: "http://localhost:8080/api/Category/all",
+        type:"GET",
+        dataType: 'json',
+        success: function(respuesta){
+            pintarDatos(respuesta);
+        }
     });
-};
+}
 
 function pintarDatos(datos){
     let html="";
-    html += "<tr>";
-    Object.keys(datos[0]).forEach(element  =>{
-        html += "<th>"+element+"</th>";
-    });
-    html += "</tr>";
-    
+    html+="<tr>";
+    Object.keys(datos[0]).forEach(elemento => {
+        html+="<th>"+elemento+"</th>";
+    })
+    html+="</tr>";
+
+    for(let i=0;i<datos.length;i++){
+        html+="<tr>";
+        Object.values(datos[i]).forEach(elemento => {
+            html+="<td>"+elemento+"</td>";
+        })
+        html+="</tr>";
+    }
+
     $("#tabla").empty();
     $("#tabla").append(html);
-    
 }
-
